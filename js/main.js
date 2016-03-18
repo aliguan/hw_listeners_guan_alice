fccdfe51a81a4d1159bfea47c2ca0494a0760b79
Alice Guan <aliguan726@gmail.com>
2016-03-18 00:55:29 -0400

    log inputted form values to console

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
 formsub.addEventListener( 'submit', function( evt ) {
     evt.preventDefault();
+    console.log( 'The firstName is: ' +
+    document.getElementById( 'formsub' ).elements.namedItem( 'firstName' ).value );
+    console.log( 'The lastName is: ' +
+    document.getElementById( 'formsub' ).elements.namedItem( 'lastName' ).value );
+    console.log( 'The email is: ' +
+    document.getElementById( 'formsub' ).elements.namedItem( 'email' ).value );
+    console.log( 'The message is: ' +
+    document.getElementById( 'formsub' ).elements.namedItem( 'message' ).value );
 } );
