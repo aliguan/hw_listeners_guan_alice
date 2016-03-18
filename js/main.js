083b644a2fb72cd3003bd403acfea091d33d8dd4
Alice Guan <aliguan726@gmail.com>
2016-03-18 00:14:16 -0400

    style form

--- js/main.js
+++
@@ -1,8 +0,0 @@
-var x = 1;
-
-headline.addEventListener( 'click', function addHTML() {
-    var newParagraph = document.createElement( 'p' );
-    newParagraph.innerText = 'This is click number ' + x;
-    document.getElementsByClassName( 'container' )[ 0 ].appendChild( newParagraph );
-    x++;
-} );
