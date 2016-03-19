0f9b9a5eb5fd0c861d6f7a45520dbf4e67c9407d
Alice Guan <aliguan726@gmail.com>
2016-03-18 20:48:15 -0400

    change h1 heading color when link is clicked

--- js/main.js
+++ js/main.js
@@ -9,3 +9,11 @@ pup.addEventListener( 'click', function addpic() {
     /* select the first paragraph and add the picture after that element */
     document.getElementsByClassName( 'para' )[ 0 ].appendChild( bigheeler );
 } );
+
+/* listens for click on link */
+wikilink.addEventListener( 'click', function( evt ) {
+    /* prevents link from opening wikipedia page */
+    evt.preventDefault();
+    /* changes h1 headings to purple by adding the invert class */
+    document.getElementsByTagName( 'h1' )[ 0 ].setAttribute( 'class', 'invert' );
+} );
